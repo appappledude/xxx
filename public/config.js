c2d0d8626f6921380182990d527ca3006e918fae
appappledude <150356780+appappledude@users.noreply.github.com>
2023-11-09 15:21:23 +0000

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "httphandlder.appappledude.workers.dev",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.appappledude.workers.dev",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmQ5Y2IyOTMyMzUxY2M2ZjkyNDk0MmY5ZWI3MWE5NCIsInN1YiI6IjY1NDgzYzVkZDU1YzNkMDBjNWJmMTRkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NHVdG0fu5mgs_JJtgJt4HmgoBAf9OaocDVJKjh4-XPo"
 };
